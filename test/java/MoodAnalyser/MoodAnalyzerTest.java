553dfd2a94589a83ae1ccbef856d8b1934c3be99
Sangita <pal07sangita@gmail.com>
2020-10-27 10:31:38 +0530

    refactor: initialized message,Created 2 constructor for Default using Parameter

---
+++ test/java/MoodAnalyser/MoodAnalyzerTest.java
@@ -0,0 +1,36 @@
+package MoodAnalyser;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MoodAnalyzerTest {
+
+    private String mood;
+    private MoodAnalyzer moodCheck;
+
+    
+      //Testing Sad Mood
+      //Refactor: Passing parameter while creating MoodAnalyzer Object
+      //Removed parameter while calling analyzeMood Method
+     
+    
+      @Test
+    public void testSadMood() {
+        moodCheck = new MoodAnalyzer("I am in Sad Mood");
+        mood = moodCheck.analyzeMood();
+        Assert.assertThat(mood, CoreMatchers.is("SAD"));
+    }
+
+    
+     //Testing Happy Mood
+     //Refactor: Passing parameter while creating MoodAnalyzer Object
+     //Removed parameter while calling analyzeMood Method
+     
+    @Test
+    public void testHappyMood() {
+        moodCheck = new MoodAnalyzer("I am in Any Mood");
+        mood = moodCheck.analyzeMood();
+        Assert.assertThat(mood, CoreMatchers.is("HAPPY"));
+    }
+}
